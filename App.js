045a93dd41d01a05831599c1df100dfbfc23eb92
ngc <nora.gonzalo@gmail.com>
2021-01-08 18:49:36 +0100

    ADD components folder-refactorized App.js

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Modal } from 'react-native';
-import MapView from 'react-native-maps'
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { Map, Modal } from './Components/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>
-      <Modal
-        animationType='slide'
-        transparent={true}
-        visible={true}
-      >
-        <View style={styles.center}>
-          <View style={styles.modalView}>
-            <Text>😊</Text>
-          </View>
-        </View>
-      </Modal>
+      <Map />
+      <Modal/>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,24 +20,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  map:{
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width,
 
-  },
-  center:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  modalView:{
-    backgroundColor:'#fff',
-    borderRadius:4,
-    padding:20,
-    shadowColor:'#000',
-    shadowOffset:{
-      width: 0,
-      height:3,
-    }
-  }
+
 });
