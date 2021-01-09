bf639c3277eb0e80fd8d7c5b9021092ece5c72e6
ngc <nora.gonzalo@gmail.com>
2021-01-10 00:30:21 +0100

    ADD set marker on modal-input

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import { Map, Modal, Panel } from './Components/index';
+import { Map, Modal, Panel, Input } from './Components/index';
 
 export default function App() {
 const [puntos, setPuntos] = useState([])
+const [nombre, setNombre] = useState("")
+const [puntoTemp, setPuntoTemp] = useState({})
+const [visibility, setVisibility] = useState(false)
+
+
+
 const handleLongPress = ({nativeEvent})=> {
-  const newPuntos = puntos.concat({coordinate: nativeEvent.coordinate})
-  setPuntos(newPuntos)
+  // const newPuntos = puntos.concat({coordinate: nativeEvent.coordinate})
+  // setPunto(newPuntos)
+  setPuntoTemp(nativeEvent.coordinate)
+  setVisibility(true)
   }
-  console.log(puntos)
+  // console.log(puntos)
+
+  const handleChangeText = text => {
+    setNombre(text)
+  }
   return (
     <View style={styles.container}>
       <Map onLongPress={handleLongPress}/>
       <Panel />
-      <Modal visibility={true}>
-    <Text>Hola</Text>
+      <Modal visibility={visibility}>
+    <Input title="Nombre" placeholder="nombre del punto" onChangeText={handleChangeText}></Input>
       </Modal>
 
       <StatusBar style="auto" />
